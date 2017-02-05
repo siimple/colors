2f499fb434994917cfbe74398d4424d257cd88c9
jmjuanes <josemijuanes@gmail.com>
2017-02-05 16:48:49 +0100

    gulpfile.js: init

---
+++ gulpfile.js
@@ -0,0 +1,92 @@
+//Import dependencies
+var autoprefixer = require('gulp-autoprefixer');
+var fs = require('fs');
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+var header = require('gulp-header');
+var sass = require('gulp-sass');
+var handlebars = require('gulp-compile-handlebars');
+
+//Import the package
+var pkg = require('./package.json');
+
+//Import data
+var data = require('./data.json');
+
+//Banner structure
+var banner = []
+banner.push('/**');
+banner.push(' * <%= pkg.name %> - <%= pkg.description %>');
+banner.push(' * @version v<%= pkg.version %>');
+banner.push(' * @link <%= pkg.homepage %>');
+banner.push(' * @license <%= pkg.license %>');
+banner.push('**/');
+banner.push(' ');
+banner.push(' ');
+
+//Join the banner
+banner = banner.join('\n');
+
+//Compile the colors palette
+gulp.task('compile', function()
+{
+  //Handlebars options
+  var opt = { helpers: {} };
+
+  //Add the capitalize text helper
+  opt.helpers.capitalize = function(text){ return text.charAt(0).toUpperCase() + text.substring(1); };
+
+  //Compile the handlebars files
+  return gulp.src('./templates/**.hbs')
+
+  //Compile the handlears files
+  .pipe(handlebars(data, opt))
+
+  //Rename the file
+  .pipe(rename({ extname: '.scss' }))
+
+  //Save
+  .pipe(gulp.dest('./src/'));
+});
+
+//Build the SCSS files
+gulp.task('build', function()
+{
+  //Select all the SCSS files
+  gulp.src('src/**/*.scss')
+
+  //Build the scss files
+  .pipe(sass().on('error', sass.logError))
+
+  //Autoprefix
+  .pipe(autoprefixer({ browsers: ['last 3 versions', 'IE 9'], cascade: false }))
+
+  //Add the header
+  .pipe(header(banner, { pkg : pkg } ))
+
+  //Save on the dist folder
+  .pipe(gulp.dest('./dist/'));
+});
+
+//Minimize the css
+gulp.task('minimize', function()
+{
+  //Set the source file
+  gulp.src('dist/siimple-colors.css')
+
+  //Clean the css
+  .pipe(cleanCSS({ compatibility: '*', processImportFrom: ['!fonts.googleapis.com'] }))
+
+  //Rename the file
+  .pipe(rename('siimple-colors.min.css'))
+
+  //Add the header
+  .pipe(header(banner, { pkg : pkg } ))
+
+  //Save on the dist folder
+  .pipe(gulp.dest('dist/'));
+});
+
+//Execute the tasks
+gulp.task('default', [ 'build', 'minimize' ]);
