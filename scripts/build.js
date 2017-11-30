c02a56db1f1a68b6b063b72061e03b03823932eb
jmjuanes <josemijuanes@gmail.com>
2017-11-30 19:39:09 +0100

    scripts/build.js: added minimize task

--- scripts/build.js
+++ scripts/build.js
@@ -1,15 +1,39 @@
 //Import dependencies
+var path = require('path');
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var header = require('gulp-header');
+var rename = require('gulp-rename');
+var cleanCSS = require('gulp-clean-css');
 var sass = require('gulp-sass');
+var keue = require('keue');
 
 //Import configuration
 var config = require('./config.js');
 
-//Build the SCSS files
-var buildCss = function()
+//Initialize the new keue
+var k = new keue(function(next)
 {
+  //Clean the output directory
+  rmr.sync(config.dist);
+
+  //Continue
+  return next();
+});
+
+//Compile the css files
+k.then(function(next)
+{
+  //Output stream
+  var output = gulp.dest(config.dist);
+
+  //Build task completed
+  output.on('finish', function()
+  {
+    //Continue with the next task
+    return next();
+  });
+
   //Select all the SCSS files
   return gulp.src('scss/**/*.scss')
 
@@ -23,8 +47,43 @@ var buildCss = function()
   .pipe(header(config.banner, { } ))
 
   //Save on the dist folder
-  .pipe(gulp.dest(config.dist));
-};
+  .pipe(output);
+});
 
-//Build the css files
-buildCss();
+//Minimize the css files
+k.then(function(next)
+{
+  //Output stream
+  var output = gulp.dest(config.dist);
+
+  //Build task completed
+  output.on('finish', function()
+  {
+    //Continue with the next task
+    return next();
+  });
+
+  //Set the source file
+  return gulp.src(path.join(config.dist, './siimple-colors.css'))
+
+  //Clean the css
+  .pipe(cleanCSS({ compatibility: '*' }))
+
+  //Rename the file
+  .pipe(rename({ extname: '.min.css' }))
+
+  //Add the header
+  .pipe(header(config.banner, { } ))
+
+  //Save on the dist folder
+  .pipe(output);
+});
+
+//Queue finished
+k.finish(function()
+{
+  //Display in console
+  console.log('Build completed');
+});
+
+
