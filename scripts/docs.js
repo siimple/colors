9fc1a6248c6d747595cec1653edae4d835c839f6
jmjuanes <josemijuanes@gmail.com>
2018-02-23 17:52:28 +0100

    Added scss compiler. Copy vendor files. Minor changes

--- scripts/docs.js
+++ scripts/docs.js
@@ -1,5 +1,7 @@
 let gulp = require("gulp");
 let handlebars = require('gulp-compile-handlebars');
+let sass = require("gulp-sass");
+let autoprefixer = require("gulp-autoprefixer");
 let path = require("path");
 
 let config = require("./config.js");
@@ -7,15 +9,27 @@ let config = require("./config.js");
 //Build the documentation website
 let buildDocs = function () {
     let output = path.resolve(process.cwd(), "./docs/dist/");
+    let outputVendor = path.join(output, "./vendor/");
     let data = {
         version: config.getVersion()
     };
+
     //Compile the html files
     gulp.src("docs/src/**.html")
         .pipe(handlebars(data, {}))
         .pipe(gulp.dest(output));
+
+    //Compile the scss files
+    gulp.src("docs/scss/**.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(autoprefixer({ browsers: ["last 3 versions", "IE 9"], cascade: false }))
+        .pipe(gulp.dest(output));
+
     //Copy the assets files
     gulp.src("docs/assets/**/*.*", {base: "docs/"})
         .pipe(gulp.dest(output));
+
+    //Copy the vendor files
+    gulp.src("dist/siimple-colors.scss").pipe(gulp.dest(outputVendor));
 };
 buildDocs();
